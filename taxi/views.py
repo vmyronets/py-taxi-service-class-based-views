6c40837e307ab49d48b2403c45be03b29f989489
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-04-04 23:05:02 +0300

    Fix DriverDetailView in models.py

--- taxi/views.py
+++ taxi/views.py
@@ -40,4 +40,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related("manufacturer")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
